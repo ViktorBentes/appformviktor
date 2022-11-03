9a436251f28037b6854601ba424834dbbdf30223
ViktorBentes <viktorbentes@gmail.com>
2022-11-03 19:04:12 -0300

    App Form para para testes

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bytebank2/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+        colorScheme: ColorScheme.fromSwatch()
+            .copyWith(secondary: Colors.blueAccent[700]),
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
